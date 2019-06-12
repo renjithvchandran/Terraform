2b01efae45e08ae16d2cb3801e6c257b5cf83eed
Renjith Chandran <51434515+renjithvchandran@users.noreply.github.com>
2019-06-12 23:40:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
+  project = "phrasal-insight-237905"
+  region  = "asia-south1"
   credentials = "${file("./creds/serviceaccount.json")}"
-  project     = "gke-tf-demo"
-  region      = "europe-west1"
-}
+  }
