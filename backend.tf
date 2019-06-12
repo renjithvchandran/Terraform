3dac5289c0e4d6b390f4861333d9fe0a3e276bf2
Renjith Chandran <51434515+renjithvchandran@users.noreply.github.com>
2019-06-12 22:40:23 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "phrasal-insight-237905-tfstate"
-    credentials = "./creds/serviceaccount.json"
+    credentials = "${file("./creds/serviceaccount.json")}"
   }
 }
