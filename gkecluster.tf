a82caf02a705739cf43be9bc7471007ddedd9fe7
root <renjith.v.chandran@gmail.com>
2019-07-05 11:47:30 +0000

    new cluster

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "gke-cluster"{
-  name               = "my-kube-test_new"
+  name               = "my-kube-test-new"
   network            = "default"
   location           = "europe-west1-b"
   initial_node_count = 2
