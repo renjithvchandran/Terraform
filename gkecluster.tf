438a772a241fc27e025dd2c91662da8f644cd4c2
root <renjith.v.chandran@gmail.com>
2019-07-05 12:27:18 +0000

    increase node and name change

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "gke-cluster"{
-  name               = "my-kube-test-new"
+  name               = "my-kube-test"
   network            = "default"
   location           = "europe-west1-b"
-  initial_node_count = 2
+  initial_node_count = 3
 }
