dd1f23126716bc8c52da8c1e96235ab7a2dc1cac
root <renjith.v.chandran@gmail.com>
2019-07-05 09:45:30 +0000

    changed jenkinsfile

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,4 +1,4 @@
-resource "google_container_cluster" "my-kube-test"{
+resource "google_container_cluster" "gke-cluster"{
   name               = "my-kube-test"
   network            = "default"
   location           = "europe-west1-b"
