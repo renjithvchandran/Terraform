259629e308ebc3449d27bc192c0689186215eee3
Renjith Chandran <51434515+renjithvchandran@users.noreply.github.com>
2020-06-01 18:14:59 +0530

    Update gkecluster.tf

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "gke-cluster"{
-  name               = "my-kube-test_dev"
+  name               = "my-kube-test_devp"
   network            = "default"
   location           = "us-central1-a"
   initial_node_count = 3
